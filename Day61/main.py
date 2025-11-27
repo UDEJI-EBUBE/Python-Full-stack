6db5db5dfa78d06ed090dc820e33fd3c01388cca
UDEJI-EBUBE <udejiebube13@gmail.com>
2025-11-27 22:52:09 +0100

    Add files via upload

---
+++ Day61/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, length
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap5
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField("Log In")
+
+app = Flask(__name__)
+app.secret_key = "avatar"
+bootstrap = Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+# main.py
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.password.data == "12345678" and form.email.data == "admin@email.com":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
