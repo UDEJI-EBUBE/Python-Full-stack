88fc0bcf49396663af5a2ef510d25f88526c663b
UDEJI-EBUBE <udejiebube13@gmail.com>
2025-11-27 22:54:43 +0100

    Add files via upload

---
+++ Day62/main.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+import csv
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    Location_URL=StringField('Cafe Location on google map', validators=[DataRequired()])
+    open_time=StringField('Opening Time e.g 8am', validators=[DataRequired()])
+    closing_time=StringField('Closing Time e.g 5:30pm', validators=[DataRequired()])
+    coffee_rating=SelectField('Coffee Rating',
+                        choices=[ "☕️" * (i + 1) for i in range(5)],
+                        validators=[DataRequired()])
+    wifi_rating=SelectField('WIFI Strength Rating',
+                        choices = ['✘'] + ["💪" * (i + 1) for i in range(5)],
+                        validators=[DataRequired()])
+    power_outlet_rating=SelectField('Power Outlet Availability',
+                        choices = ['✘'] + ["🔌" * (i + 1) for i in range(5)],
+                        validators=[DataRequired()])
+
+    submit = SubmitField('Submit')
+
+# Exercise:
+# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
+# make coffee/wifi/power a select element with choice of 0 to 5.
+#e.g. You could use emojis ☕️/💪/✘/🔌
+# make all fields required except submit
+# use a validator to check that the URL field has a URL entered.
+# ---------------------------------------------------------------------------
+
+
+# all Flask routes below
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        print("True")
+        with open('cafe-data.csv', 'a', newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            new_row_data = [
+                form.cafe.data,
+                form.Location_URL.data,
+                form.open_time.data,
+                form.closing_time.data,
+                form.coffee_rating.data,
+                form.wifi_rating.data,
+                form.power_outlet_rating.data
+            ]
+            csv_writer.writerow(new_row_data)
+            return redirect(url_for('cafes'))
+    # Exercise:
+    # Make the form write a new row into cafe-data.csv
+    # with   if form.validate_on_submit()
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+        print(list_of_rows)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
